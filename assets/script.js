40a4ffcbcced712ae5915e2bcac2b1826899c5d2
BlueAngler <dannyo1028@gmail.com>
2022-08-25 15:35:19 -0500

    added city search function

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,43 @@
 var APIKey = "4cbc52e85e24ca7452641b3f8eee29fb"; 
-var city = ""; 
+ 
 var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey
 var searchbuttonEl = document.getElementById("searchbutton");
 var currentweatherEl = document.getElementById("currentweather")
 var searchedcity = document.getElementById("cityentered")
-
-// fetch(queryURL)
-//   .then(function (response) {
-//     return response.json();
-//   })
-//   .then(function (data) {
-//     console.log(data);
-//     currentweatherEl.textContent=response.main.temp
-
-//   })
+var city = "";
 
 
 
-  var city=searchedcity.value
-console.log(searchedcity.value)
+
+searchbuttonEl.addEventListener('click', searchcitybutton)
+function searchcitybutton() {
+    var citysearch=searchedcity.value
+    console.log("The value of the searched city is:" , citysearch)
+    if(city="") {
+        city=citysearch
+    }
+        
+   
+   
+
+
+
+
+
+
+   fetch(queryURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+        currentweatherEl.textContent=response.main.temp
+
+    })
+}
+
+
+ 
+
+
+
